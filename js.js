572e7fd6304184730acf6067a57784ca96cc5805
h3rb <hgilliland@gmail.com>
2022-01-20 15:55:36 -0500

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,64 @@
 // js.js Copyright (c) 2020 Herbert E. Gilliland III. 
 // MIT License.
 
-var jsjsGlobal=[];
-
 class JsJs {
   
   constructor() {
+    this.events = [
+      "Init",
+      "Update",
+      "Get",
+      "Put",
+      "Store",
+      "Retrieve",
+      "Serialize",
+      "Parse",
+      "Encode"
+      "Decode"
+    ];
+    this.registered = {};
+    for ( var i=0; i<events.length; i++ ) this.registered[events[i]]=[];
+  }
+  
+  // Add a new event name to the global event tracker.
+  add(name) {
+    if ( this.get(name) == null ) {
+      this.events[this.events.length]=name;
+      return true;
+    }
+    return false;
+  }
+  
+  index(name) {
+    for ( var i=0; i<this.events.length; i++ ) {
+      if ( this.events[i].toLowerCase() == name.toLowerCase() ) return i;
+    }
+    return -1;
+  }
+  
+  // Get back a list of registered events
+  get(name) {
+    var parsed=parseInt(name);
+    if ( !isNaN(parsed) ) {
+      if ( parsed >= 0 && parsed < this.events.length ) return this.registered[i];
+    } else {
+      parsed = this.index(name);
+      if ( parsed != -1 ) return this.registered[parsed];
+    }
+    return null;
+  }
+  
+  run(name) {
+    var list=null;
+    var parsed=parseInt(name);
+    if ( !isNaN(parsed) ) {
+      if ( parsed >= 0 && parsed < this.events.length ) list=this.registered[i];
+    } else {
+      parsed = this.index(name);
+      if ( parsed != -1 ) list=this.registered[parsed];
+    }
+    if ( !list ) return;
+    list.forEach( eventfun => eventfun() );
   }
   
  // Merges an object with another
@@ -23,6 +76,13 @@ class JsJs {
   extend(to, from) {
     return merge(to,from);
   }
+  
+  // Returns an empty component class
+  prototype() {
+    var o={};
+    for ( var i=0; i<this.events.length; i++ ) o[this.events[i]]=function(){};
+    return o;
+  }
     
   // Imports an object as a global component
   component(to, from,name=null) {
@@ -34,10 +94,10 @@ class JsJs {
    }
   }
 
-}
+};
 
 
 // Your global library programmer shortcut.
 
-var _$ = new Jaseb();
+var _$ = new JsJs();
 var jsjs=_$;
